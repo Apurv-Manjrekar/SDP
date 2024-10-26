48de21bb59a3d18f32802f8cc645ab6448a366d0
Apurv-Manjrekar <apurv.manjrekar05@gmail.com>
2024-10-26 19:42:01 -0400

    modifying diffprivlib test

--- compare_tools/IBM_diffprivlib/IBM_diffprivlib_test.py
+++ compare_tools/IBM_diffprivlib/IBM_diffprivlib_test.py
@@ -32,39 +32,58 @@ def load_uci_adult_dataset():
     
     return data
 
-def logistic_regression_test(data):
+def logistic_regression_test(data, num_trials, epsilon):
     X = data[["age", "education-num", "hours-per-week", "capital-gain", "capital-loss"]]
     y = data["income_binary"]
 
-    # Split and scale the data
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-    scaler = StandardScaler()
-    X_train = scaler.fit_transform(X_train)
-    X_test = scaler.transform(X_test)
+    base_model_avg_time = 0
+    dp_model_avg_time = 0
+    base_model_avg_accuracy = 0
+    dp_model_avg_accuracy = 0
 
-    # Train DP Logistic Regression model
-    dp_model_start = time.time()
-    dp_model = LogisticRegression(epsilon=100)
-    dp_model.fit(X_train, y_train)
-    dp_model_time = time.time() - dp_model_start
+    for i in range(num_trials):
+        # Split and scale the data
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+        scaler = StandardScaler()
+        X_train = scaler.fit_transform(X_train)
+        X_test = scaler.transform(X_test)
 
-    # Train non-private logistic regression model
-    base_model_start = time.time()
-    base_model = base_LogisticRegression()
-    base_model.fit(X_train, y_train)
-    base_model_time = time.time() - base_model_start
+        # Train DP Logistic Regression model
+        dp_model_start = time.time()
+        dp_model = LogisticRegression(epsilon=epsilon)
+        dp_model.fit(X_train, y_train)
+        dp_model_time = time.time() - dp_model_start
+
+        # Train non-private logistic regression model
+        base_model_start = time.time()
+        base_model = base_LogisticRegression()
+        base_model.fit(X_train, y_train)
+        base_model_time = time.time() - base_model_start
+
+        base_model_avg_time += base_model_time
+        dp_model_avg_time += dp_model_time
+        
+        y_pred = base_model.predict(X_test)
+        base_model_accuracy = accuracy_score(y_test, y_pred)
+        dp_model_accuracy = dp_model.score(X_test, y_test)
+
+        base_model_avg_accuracy += base_model_accuracy
+        dp_model_avg_accuracy += dp_model_accuracy
+    
+    base_model_avg_accuracy /= num_trials
+    dp_model_avg_accuracy /= num_trials
+    base_model_avg_time /= num_trials
+    dp_model_avg_time /= num_trials
 
     # Display time taken
-    print("IBM Diffprivlib DP Logistic Regression Time Taken:", dp_model_time)
-    print("Base Case Logistic Regression Time Taken:", base_model_time)
+    print(f"IBM Diffprivlib DP Logistic Regression Time Taken (Epsilon: {epsilon}):", dp_model_avg_time)
+    print("Base Case Logistic Regression Time Taken:", base_model_avg_time)
 
     # Evaluate models
-    print("IBM Diffprivlib DP Logistic Regression accuracy:", dp_model.score(X_test, y_test))
-    y_pred = base_model.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    print("Base Case Logistic Regression Accuracy:", accuracy)
-
+    print(f"IBM Diffprivlib DP Logistic Regression accuracy (Epsilon: {epsilon}):", dp_model_avg_accuracy)
+    print("Base Case Logistic Regression Accuracy:", base_model_avg_accuracy)
 
 if __name__ == '__main__':
     data = load_uci_adult_dataset()
-    logistic_regression_test(data)
+    for epsilon in [0.01, 1, 100]:
+        logistic_regression_test(data, 50, epsilon)
